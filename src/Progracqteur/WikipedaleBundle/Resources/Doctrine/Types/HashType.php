407d8e724147d2634832d2ce0f4388131713d842
Julien Fastré <julien@fastre.info>
2012-04-29 23:44:52 +0200

    complète type doctrine HashType

--- src/Progracqteur/WikipedaleBundle/Resources/Doctrine/Types/HashType.php
+++ src/Progracqteur/WikipedaleBundle/Resources/Doctrine/Types/HashType.php
@@ -4,6 +4,7 @@ namespace Progracqteur\WikipedaleBundle\Resources\Doctrine\Types;
 
 use Doctrine\DBAL\Types\Type; 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Progracqteur\WikipedaleBundle\Resources\Container\Hash;
 
 /**
  * A Type for Doctrine to implement the hstore capabilities
@@ -13,10 +14,73 @@ use Doctrine\DBAL\Platforms\AbstractPlatform;
  */
 class HashType extends Type {
     
-    cont HASH = 'hash';
+    const HASH = 'hash';
     
-    public function getName(){
+    public function getName()
+    {
         return self::HASH;
+    }
+    
+    /**
+     *
+     * @param array $fieldDeclaration
+     * @param AbstractPlatform $platform
+     * @return type 
+     */
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'xml';
+    }
+    
+    /**
+     *
+     * @param Hash $value
+     * @param AbstractPlatform $platform
+     * @return Progracqteur\WikipedaleBundle\Resources\Container\Hash 
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        $dom = new \DOMDocument();
+        $dom->loadXML($value);
+        
+        $h = new Hash();
+        
+        if ($dom->hasChildNodes())
+        {
+            foreach ($dom->childNodes as $node)
+            {
+                $h->__set($node->nodeName, $node->nodeValue);
+            }
+        }
+        
+        return $h;
+        
+        
+    }
+    
+    public function convertToDatabaseValue($hash, AbstractPlatform $platform)
+    {
+        $dom = new \DOMDocument();
+        
+        foreach ($hash as $key => $value) {
+            $node = $dom->createElement($key, $value);
+            $dom->appendChild($node);
+        }
+        return $dom->saveXML();
+        
+        /*$str = 'hstore(ARRAY['; 
+        $i = 0;
+        foreach ($hash as $key => $value)
+        {
+            if ($i > 0) {
+                $str .= ',';
+            }
+            $str .= '[\''.$key.'\',\''.$value.'\']';
+        }
+        
+        $str.='])';
+        return $str;*/
+        
     }
     
     
