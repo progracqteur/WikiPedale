faa839fc1b1aee6190f8ebf4c9a10ceef5124bed
Julien Fastré <julien@fastre.info>
2012-06-03 22:20:18 +0200

    implémente requête par ville de manière grossière (créer un bbox autour de ces villes, coordonnées des bbox inscrits dans le code) pour LIège et Mons

--- src/Progracqteur/WikipedaleBundle/Controller/PlaceController.php
+++ src/Progracqteur/WikipedaleBundle/Controller/PlaceController.php
@@ -49,6 +49,20 @@ class PlaceController extends Controller {
     }
     
     public function listByBBoxAction($_format, Request $request){
+        
+        /*TODO la requête actuelle crée un rectangle grossier autour 
+         * des limites de la commune pour extraire les place à l'intérieur de cette limite
+         * 
+         * La finesse de cette requete pourra être améliorée lorsque les coordonnées géographiques
+         * des limites de communes auront été ajoutées au système
+         * 
+         * 
+         */
+        
+        /* TODO
+         * Pour l'instant, seuls Mons et Liège sont disponible dans cette requete. Ajouter 
+         * d'autres villes si nécessaire.
+         */
         $em = $this->getDoctrine()->getEntityManager();
         
         $BboxStr = $request->get('bbox', null);
@@ -69,6 +83,49 @@ class PlaceController extends Controller {
         
         
         $bbox = BBox::fromCoord($BboxArr[0], $BboxArr[1], $BboxArr[2], $BboxArr[3]);
+        
+        $p = $em->createQuery('SELECT p from ProgracqteurWikipedaleBundle:Model\\Place p where covers(:bbox, p.geom) = true')
+                ->setParameter('bbox', $bbox->toWKT());
+        
+        $r = $p->getResult();
+        
+        switch($_format) {
+            case 'json':
+                $jsonencoder = new JsonEncoder();
+                $serializer = new Serializer(array(new CustomNormalizer()) , array('json' => $jsonencoder));
+		$rep = array('results' => $r);
+                $ret = $serializer->serialize($rep, $_format);
+                return new Response($ret);
+                break;
+            case 'html':
+                return new Response('Pas encore implémenté');
+                
+        }
+        
+        
+    }
+    
+    public function listByCityAction($_format, Request $request)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $city = $request->get('city', null);
+        if ($city === null)
+        {
+            throw new \Exception('Renseigner une ville dans une variable \'city\' ');
+        }
+        
+        
+        switch($city) {
+            case 'mons':
+                $bbox = BBOX::fromCoord(50.515173, 4.086857, 50.374591, 3.852024);
+                break;
+            case 'liege':
+                $bbox = BBOX:: fromCoord(50.6910, 5.678158, 50.559469, 5.520229);
+                break;
+            default:
+                throw new \Exception("La ville renseignée ('$city') n'est pas connue du système");
+        }
         
         $p = $em->createQuery('SELECT p from ProgracqteurWikipedaleBundle:Model\\Place p where covers(:bbox, p.geom) = true')
                 ->setParameter('bbox', $bbox->toWKT());
