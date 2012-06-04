9c2fe93b2e868ebeb215c9f2bdc6880f1abe680e
Julien Fastré <julien@fastre.info>
2012-06-04 16:19:43 +0200

    implémentation de la localisation de l'utilisateur

--- src/Progracqteur/WikipedaleBundle/Controller/ManagerController.php
+++ src/Progracqteur/WikipedaleBundle/Controller/ManagerController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Progracqteur\WikipedaleBundle\Entity\Model\Place;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Progracqteur\WikipedaleBundle\Resources\Geo\Point;
 
 /**
  * Description of PlaceController
@@ -16,8 +17,49 @@ class ManagerController extends Controller {
     
     public function toCityAction($city, Request $request)
     {
+        $session = $this->getRequest()->getSession();
+	
+	switch ($city) {
+		case 'mons' :
+			$point = Point::fromLonLat(3.971868, 50.452449);
+			$session->set('city_center', $point);
+                        $session->set('city', 'mons');
+			break;
+                case 'liege' :
+                    $point = Point::fromLonLat(5.60259, 50.612973);
+                    $session->set('city_center', $point);
+                    $session->set('city', 'liege');
+                    break;
+                default:
+                    throw new Exception("Le nom de la ville demandé ('$city') est inconnu dans notre système");
+    	}
         
-    }
+        $url = $request->get('url', null);
+        //TODO: attention: il faut vérifier que l'URL est bien une URL du site, sinon risque de renvoyer n'importe où
+        
+        if ($url === null)
+        {
+            $url = $this->generateUrl('wikipedale_homepage');
+        }
+        
+        return $this->redirect($url);
+        
+     }
+     
+     public function resetCityAction() {
+         $session = $this->getRequest()->getSession();
+         $session->set('city', null);
+         $session->set('city_center', null);
+         
+         $url = $request->get('url', null);
+        
+        if ($url === null)
+        {
+            $url = $this->generateUrl('wikipedale_homepage');
+        }
+        
+        return $this->redirect($url);
+     }
     
 }
 
