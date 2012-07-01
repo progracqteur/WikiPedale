0ffdee58846076c16fc8bc696830b16aca8e8651
Julien Fastré <julien@fastre.info>
2012-07-01 22:18:01 +0200

    supprime actions de débogages

--- src/Progracqteur/WikipedaleBundle/Resources/Security/ChangeException.php
+++ src/Progracqteur/WikipedaleBundle/Resources/Security/ChangeException.php
@@ -14,13 +14,11 @@ class ChangeException extends AccessDeniedException{
     
     public static function param($param)
     {
-        throw new \Exception('param');
         return new self("L'utilisateur ne peut pas modifier le paramètre $param");
     }
     
     public static function mayNotCreateEntityForAnotherUser()
     {
-        throw new \Exception('param');
         return new self("L'utilisateur ne peut pas créer une entité sous le nom d'un autre utilisateur");
     }
     
