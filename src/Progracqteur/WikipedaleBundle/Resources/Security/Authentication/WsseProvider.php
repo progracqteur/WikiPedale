df87258c9e5bee6ef7f81ff3d643b183e453cdc0
Julien Fastré <julien@fastre.info>
2012-08-23 20:37:56 +0200

    début test implémentation wsse

---
+++ src/Progracqteur/WikipedaleBundle/Resources/Security/Authentication/WsseProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Progracqteur\WikipedaleBundle\Resources\Security\Authentication;
+
+use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\NonceExpiredException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Progracqteur\WikipedaleBundle\Resources\Security\Authentication\WsseUserToken;
+
+/**
+ * Description of WsseProvider
+ *
+ * @author Julien Fastré <julien arobase fastre point info>
+ */
+class WsseProvider implements AuthenticationProviderInterface {
+    
+    
+    
+}
+
---
+++ src/Progracqteur/WikipedaleBundle/Resources/Security/Firewall/WsseListener.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Progracqteur\WikipedaleBundle\Resources\Security\Firewall;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Http\Firewall\ListenerInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Progracqteur\WikipedaleBundle\Resources\Security\Authentication\WsseUserToken;
+
+/**
+ * Description of WsseListener
+ *
+ * @author Julien Fastré <julien arobase fastre point info>
+ */
+class WsseListener implements ListenerInterface {
+    
+    protected $securityContext;
+    protected $authenticationManager;
+
+    public function __construct(SecurityContextInterface $securityContext, AuthenticationManagerInterface $authenticationManager)
+    {
+        $this->securityContext = $securityContext;
+        $this->authenticationManager = $authenticationManager;
+    }
+    
+    public function handle(GetResponseEvent $event) {
+        $request = $event->getRequest();
+
+        if ($request->headers->has('x-wsse')) {
+
+            $wsseRegex = '/UsernameToken Username="([^"]+)", PasswordDigest="([^"]+)", Nonce="([^"]+)", Created="([^"]+)"/';
+
+            if (preg_match($wsseRegex, $request->headers->get('x-wsse'), $matches)) {
+                $token = new WsseUserToken();
+                $token->setUser($matches[1]);
+
+                $token->digest   = $matches[2];
+                $token->nonce    = $matches[3];
+                $token->created  = $matches[4];
+
+                try {
+                    $returnValue = $this->authenticationManager->authenticate($token);
+
+                    if ($returnValue instanceof TokenInterface) {
+                        return $this->securityContext->setToken($returnValue);
+                    } elseif ($returnValue instanceof Response) {
+                        return $event->setResponse($returnValue);
+                    }
+                } catch (AuthenticationException $e) {
+                    // you might log something here
+                }
+            }
+        }
+
+        $response = new Response();
+        $response->setStatusCode(403);
+        $event->setResponse($response);
+    }
+}
+
