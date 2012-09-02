153c9f0751dddc1b0df402cd5dc4ddb054f33160
Julien Fastré <julien@fastre.info>
2012-09-02 16:43:26 +0200

    Messages d'erreurs plus explicites dans la gestion des photos

--- src/Progracqteur/WikipedaleBundle/Controller/PhotoController.php
+++ src/Progracqteur/WikipedaleBundle/Controller/PhotoController.php
@@ -88,7 +88,11 @@ class PhotoController extends Controller
             
             if (!$uploadedFile->isValid())
             {
-                throw new \Exception("Erreur d'envoi de fichier. Vérifiez la taille du fichier");
+                $r = new Response("Erreur d'envoi de fichier. Vérifiez la taille du fichier. Erreur du fichier :"
+                        .$uploadedFile->getError()." ".
+                        $this->file_upload_error_message($uploadedFile->getError()));
+                $r->setStatusCode(413);
+                return $r;
             }
             
             if ($form->isValid()) //TODO créer une formulaire de validation
@@ -171,5 +175,26 @@ class PhotoController extends Controller
         ));
         
     }
+    
+    private function file_upload_error_message($error_code) {
+        switch ($error_code) {
+            case UPLOAD_ERR_INI_SIZE:
+                return 'The uploaded file exceeds the upload_max_filesize directive in php.ini';
+            case UPLOAD_ERR_FORM_SIZE:
+                return 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
+            case UPLOAD_ERR_PARTIAL:
+                return 'The uploaded file was only partially uploaded';
+            case UPLOAD_ERR_NO_FILE:
+                return 'No file was uploaded';
+            case UPLOAD_ERR_NO_TMP_DIR:
+                return 'Missing a temporary folder';
+            case UPLOAD_ERR_CANT_WRITE:
+                return 'Failed to write file to disk';
+            case UPLOAD_ERR_EXTENSION:
+                return 'File upload stopped by extension';
+            default:
+                return 'Unknown upload error';
+        }
+    } 
 }
 
