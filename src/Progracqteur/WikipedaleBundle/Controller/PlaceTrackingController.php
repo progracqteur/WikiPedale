136886ed8368aec796bff384396a81acdf2fab56
Julien Fastré <julien@fastre.info>
2012-09-19 23:04:32 +0200

    creation controlleur placeTracking

---
+++ src/Progracqteur/WikipedaleBundle/Controller/PlaceTrackingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Progracqteur\WikipedaleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * 
+ *
+ * @author Julien Fastré <julien arobase fastre point info>
+ */
+class PlaceTrackingController extends Controller {
+    
+    public function byCityAction($slug, Request $request)
+    {
+        
+    }
+    
+}
+
