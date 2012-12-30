1f9a6d0101a878a5f500efd7d33d728cdc1db8e4
Marc Ducobu <marc.ducobu@gmail.com>
2012-12-30 15:05:09 +0100

    #53 verification du mail

--- web/js/form_informer.js
+++ web/js/form_informer.js
@@ -4,6 +4,12 @@ var error_for_n_field = false;
 var error_for_e_field = false;
 var change_informer_fields = false;
 
+function is_mail_valid(anEmail)
+{
+	var reg = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+	return(reg.test(anEmail));
+}
+
 function reset_informer(){
 	error_for_l_field = false;
 	error_for_d_field = false;
@@ -56,7 +62,7 @@ function update_n_informer_for_form() {
 
 function update_e_informer_for_form() {
 	var l_value = jQuery('#e').attr('value');
-	if (l_value) {
+	if (l_value && is_mail_valid(l_value)) {
 		document.getElementById('form_informer_email').src="../img/form_ok.jpg";
 		error_for_e_field = false;
 	}
