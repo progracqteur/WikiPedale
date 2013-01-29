5d909bc1371d49a2388999076131a446efa98157
Julien Fastré <julien@fastre.info>
2013-01-29 22:32:06 +0100

    ajout formulaire pour manager

---
+++ src/Progracqteur/WikipedaleBundle/Form/Model/PlaceType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Progracqteur\WikipedaleBundle\Form\Model;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Doctrine\ORM\EntityRepository;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            //->add('geom')
+            ->add('description')
+            ->add(
+                    $builder->create('address', 'form', array('by_reference' => true))
+                        ->add('road', 'text')
+                        ->add('city', 'text')
+                    )
+            ->add('category', 'entity', array(
+                'class' => 'ProgracqteurWikipedaleBundle:Model\Category',
+                'multiple' => true,
+                'expanded' => false,
+                'query_builder' => function(EntityRepository $er) {  
+                return $er->createQueryBuilder('c')
+                            ->orderBy('c.label', 'ASC');
+                }
+            ))
+        ;
+    }
+
+    public function getName()
+    {
+        return 'progracqteur_wikipedalebundle_model_placetype';
+    }
+}
