642a927e6a12f0d991ec766db776a1f9e3c174c3
Julien Fastré <julien@fastre.info>
2013-02-25 17:30:35 +0100

    make the directory Files exists

---
+++ src/Progracqteur/WikipedaleBundle/DataFixtures/ORM/Files/empty.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * This file is not used in the project.
+ * It is used to make the folder "Files" exist in the github project. 
+ * 
+ * The folder "Files" is used by doctrine:fixtures:load to load photos
+ */
+?>
