70b9ce9cbd322ab572cdfec4bcbfba2919501492
Julien Fastré <julien@fastre.info>
2013-03-23 20:16:59 +0100

    ajout dépendances et modfiication paramètres

--- src/Progracqteur/WikipedaleBundle/ProgracqteurWikipedaleBundle.php
+++ src/Progracqteur/WikipedaleBundle/ProgracqteurWikipedaleBundle.php
@@ -3,7 +3,16 @@
 namespace Progracqteur\WikipedaleBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Progracqteur\WikipedaleBundle\Resources\Security\Authentication\WsseFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ProgracqteurWikipedaleBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new WsseFactory());
+    }
 }
