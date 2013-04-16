3983928628a05439b0bb5d65bccb4d77e03b60e8
Julien Fastré <julien@fastre.info>
2013-04-16 22:04:21 +0200

    commentaire - correction

--- src/Progracqteur/WikipedaleBundle/Controller/CommentController.php
+++ src/Progracqteur/WikipedaleBundle/Controller/CommentController.php
@@ -65,7 +65,7 @@ class CommentController extends Controller
         return $this->getCommentByPLaceLimit($_format, $placeId, null, $request);
     }
     
-    public function newAction($placeId, $_format, Request $request)
+    public function newbisAction($placeId, $_format, Request $request)
     {
         if ($request->getMethod() != 'POST')
         {
@@ -79,14 +79,13 @@ class CommentController extends Controller
         
         $em = $this->getDoctrine()->getEntityManager();
         
-        $place = $em->getRepository("ProgracqteurWikipedaleBundle:Model\\Place")->find($placeId);
+        $place = $em->getRepository("ProgracqteurWikipedaleBundle:Model\\Place")
+                ->find($placeId);
         
         if ($place === null)
         {
             throw $this->createNotFoundException("La place $placeId n'a pas été trouvée");
         }
-
-        
 
         $serializedJson = $request->get('entity', null);
         
@@ -97,11 +96,13 @@ class CommentController extends Controller
 
         $serializer = $this->get('progracqteurWikipedaleSerializer');
         
-        $comment = $serializer->deserialize($serializedJson, NormalizerSerializerService::COMMENT_TYPE, $_format);
+        $comment = $serializer->deserialize($serializedJson, 
+                NormalizerSerializerService::COMMENT_TYPE, 
+                $_format);
 
         $user = $this->get('security.context')->getToken()->getUser();
         if ($user instanceof User) { //si user is logger
-            $comment.setCreator($user);
+            $comment->setCreator($user);
         }
         else {
             throw new \Exception("Il faut être connecté pour ajouter un commentaire");
@@ -111,107 +112,103 @@ class CommentController extends Controller
             throw new \Exception("Vous n'avez pas le droit d'ajouter un commentaire");
         }
 
-        print "plus de verif pour les droits";
+        //print "plus de verif pour les droits";
 
         $em->persist($comment);
         $em->flush();
-    }
-    
-    
-    public function viewAction($fileNameP, $photoType, $_format, Request $request)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
         
-        $photo = $em->getRepository('ProgracqteurWikipedaleBundle:Model\Photo')
-                ->findOneBy(array('file' => $fileNameP.'.'.$photoType));
-        
-        if ($photo === null)
-        {
-            throw $this->createNotFoundException("La photo demandée n'a pas été trouvée");
-        }
-        
-        return $this->render('ProgracqteurWikipedaleBundle:Photo:view.html.twig', array(
-            'photo' => $photo,
-        ));
-    }
-    
-    public function updateAction($fileNameP, $photoType, $_format, Request $request)
-    {
-        if (!$this->get('security.context')->isGranted('ROLE_NOTATION'))
-        {
-            $this->get('session')->setFlash('notice', "Vous devez être un administrateur pour modifier une image");
-            throw new AccessDeniedException('Vous devez être authentifié pour modifier une image');
-        }
-        
-        $em = $this->getDoctrine()->getEntityManager();
-        
-        $photo = $em->getRepository('ProgracqteurWikipedaleBundle:Model\Photo')
-                ->findOneBy(array('file' => $fileNameP.'.'.$photoType));
-        
-        if ($photo === null)
-        {
-            throw $this->createNotFoundException("La photo demandée n'a pas été trouvée");
-        }
-        
-        $form = $this->createForm(
-                    $this->get('progracqteurWikipedale.form.type.photo'), $photo
-                );
-        
-        
-        if ($request->getMethod() == 'POST')
-        {
-            $form->bindRequest($request);
-            $photo->setPhotoService($this->get('progracqteurWikipedalePhotoService'));
-            
-            $uploadedFile = $form['file']->getData();
-            if ($uploadedFile != null && $uploadedFile->isValid())
-            {
-                $error = $uploadedFile->getError();
-                throw new \Exception("Le fichier envoyé n'est pas valide. Erreur : $error dump : $dump");
-            }
-            
-            if ($form->isValid())
-            {
-                $em->flush();
-                
-                $this->get('session')->setFlash('notice', "La photo a été mise à jour");
-                
-                return $this->redirect($this->generateUrl('wikipedale_photo_update', array(
-                    'fileNameP' => $photo->getFileName(),
-                    'photoType' => $photo->getPhotoType(),
+        return $this->redirect(
+                $this->generateUrl('wikipedale_comment_view', array( 
+                    'commentId' => $comment->getId(),
                     '_format' => $_format
-                    
-                    )));
-                
-            }
+                ))
+             );
+    }
+    
+    public function changeAction($_format, Request $request)
+    {
+        if ($request->getMethod() != 'POST')
+        {
+            throw new \Exception("Only post method accepted");
+        }
+
+        if (!$this->get('security.context')->getToken()->getUser() instanceof User)
+        {
+            throw new AccessDeniedException('Vous devez être un enregistré pour ajouter un commentaire');
         }
         
-        return $this->render('ProgracqteurWikipedaleBundle:Photo:form.html.twig', array(
-            'form' => $form->createView(),
-            'photo' => $photo
-        ));
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $serializedJson = $request->get('entity', null);
+        
+        if ($serializedJson === null)
+        {
+            throw new \Exception("Aucune entitée envoyée");
+        }
+
+        $serializer = $this->get('progracqteurWikipedaleSerializer');
+        
+        $comment = $serializer->deserialize($serializedJson, 
+                NormalizerSerializerService::COMMENT_TYPE, 
+                $_format);
+
+        $user = $this->get('security.context')->getToken()->getUser();
+        if ($user instanceof User) { //si user is logger
+            $comment->setCreator($user);
+        }
+        else {
+            throw new \Exception("Il faut être connecté pour ajouter un commentaire");
+        }
+
+        if (! $this->get('security.context')->isGranted(User::ROLE_NOTATION)) {
+            throw new \Exception("Vous n'avez pas le droit d'ajouter un commentaire");
+        }
+
+        //print "plus de verif pour les droits";
+
+        $em->persist($comment);
+        $em->flush();
+        
+        return $this->redirect(
+                $this->generateUrl('wikipedale_comment_view', array( 
+                    'commentId' => $comment->getId(),
+                    '_format' => $_format
+                ))
+             );
+    }
+    
+    public function viewAction($commentId, $_format)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $comment = $em->getRepository('ProgracqteurWikipedaleBundle:Model\\Comment')
+                ->find($commentId);
+        
+        if ($comment === null)
+        {
+            throw $this->createNotFoundException("comment with id $commentId not found");
+        }
+        
+        $serializer = $this->get('progracqteurWikipedaleSerializer');
+        
+        $rep = new NormalizedResponse(array($comment));
+        
+        $text = $serializer->serialize($rep);
+        
+        switch($_format) 
+        {
+            case 'json' : 
+                return new Response($text);
+                break;
+            default:
+                $r = new Response('format demandé indisponible');
+                $r->setStatusCode(400);
+                return $r;
+        }
         
     }
     
-    private function file_upload_error_message($error_code) {
-        switch ($error_code) {
-            case UPLOAD_ERR_INI_SIZE:
-                return 'The uploaded file exceeds the upload_max_filesize directive in php.ini';
-            case UPLOAD_ERR_FORM_SIZE:
-                return 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
-            case UPLOAD_ERR_PARTIAL:
-                return 'The uploaded file was only partially uploaded';
-            case UPLOAD_ERR_NO_FILE:
-                return 'No file was uploaded';
-            case UPLOAD_ERR_NO_TMP_DIR:
-                return 'Missing a temporary folder';
-            case UPLOAD_ERR_CANT_WRITE:
-                return 'Failed to write file to disk';
-            case UPLOAD_ERR_EXTENSION:
-                return 'File upload stopped by extension';
-            default:
-                return 'Unknown upload error';
-        }
-    } 
+    
+    
 }
 
